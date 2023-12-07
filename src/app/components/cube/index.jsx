ae3c2150d773b8ad49d4b5964d5b0bec056bffaf
Jimichelle <76010791+Jimichelle@users.noreply.github.com>
2023-12-07 23:07:19 +0100

    Refactor cube component materials

--- src/app/components/cube/index.jsx
+++ src/app/components/cube/index.jsx
@@ -29,22 +29,14 @@ function Cube() {
 
     });
 
-    const texture_1 = useLoader(TextureLoader, "/assets/1.jpg")
-    const texture_2 = useLoader(TextureLoader, "/assets/2.jpg")
-    const texture_3 = useLoader(TextureLoader, "/assets/3.jpg")
-    const texture_4 = useLoader(TextureLoader, "/assets/4.jpg")
-    const texture_5 = useLoader(TextureLoader, "/assets/5.jpg")
-    const texture_6 = useLoader(TextureLoader, "/assets/6.jpg")
+//    const texture_1 = useLoader(TextureLoader, "/assets/1.jpg")
+
 
     return (
         <mesh ref = {mesh}>
             <boxGeometry args={[2.5,2.5,2.5]}/>
-            <meshStandardMaterial map={texture_1} attach="material-0"/>
-            <meshStandardMaterial map={texture_2} attach="material-1"/>
-            <meshStandardMaterial map={texture_3} attach="material-2"/>
-            <meshStandardMaterial map={texture_4} attach="material-3"/>
-            <meshStandardMaterial map={texture_5} attach="material-4"/>
-            <meshStandardMaterial map={texture_6} attach="material-5"/>
+            <meshStandardMaterial color={"orange"} />
+
         </mesh>
     )
 }
