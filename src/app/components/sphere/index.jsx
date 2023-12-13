e9bb92c40029b57595e93e10978d6023e97cf4ff
Jimi <jimi.denysiak@icloud.com>
2023-12-13 22:58:37 +0100

    Add lat-long to Cartesian conversion function

--- src/app/components/sphere/index.jsx
+++ src/app/components/sphere/index.jsx
@@ -27,18 +27,35 @@ function Sphere() {
         mesh.current.rotation.x += delta * 0;
         mesh.current.rotation.y += delta * 0.25;
         mesh.current.rotation.z += delta * 0;
+    
 
     });
 
     const texture_1 = useLoader(TextureLoader, "/assets/earth-map.jpg")
 
+    function convertLatLongToCartesian(p){
+        let lat = p.lat * (Math.PI / 180);
+        let lng = p.long * (Math.PI / 180);
+
+        let z = Math.cos(lat) * Math.sin(lng);
+        let x = Math.sin(lat) * Math.sin(lng);
+        let y = Math.cos(lat);
+
+        return [x, y, z];
+    };
+
+    let pos = convertLatLongToCartesian({lat: 20, long: 10});
+    console.log(pos);
+ 
     
+
 
     return (
         <mesh ref = {mesh}>
             <sphereGeometry args={[2]}/>
             <meshStandardMaterial map={texture_1} />
-
+            {/* <mesh ref = {coordRef} position={pos.x, pos.y, pos.z} /> */}
+            <points position={[pos[7], pos[1], pos[2]]}/>
         </mesh>
     )
 }
